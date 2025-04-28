188339016d24907706d4ff8e7832e94e676143b8
Felippi <ascendinomartins2303@gmail.com>
2025-04-28 07:52:48 -0300

    first commit

---
+++ src/main/java/dev/lippo/ProjectJava/LippoSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.lippo.ProjectJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LippoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LippoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lippo/ProjectJava/LippoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lippo.ProjectJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LippoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
